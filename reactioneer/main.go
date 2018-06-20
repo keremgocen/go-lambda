a3a36c17569edb1f312d2c645aca27e4888f41bc
Kerem <ext.kgocen@riotgames.com>
2018-06-20 18:45:55 +0300

    refactor slack request struct

--- reactioneer/main.go
+++ reactioneer/main.go
@@ -22,33 +22,27 @@ var (
 	jsonHeader = map[string]string{"Content-Type": "application/json"}
 )
 
-// ItemStruct keeps slack event item details
-type ItemStruct struct {
-	Type    string `json:"type"`
-	Channel string `json:"channel"`
-	Ts      string `json:"ts"`
-}
-
-// SlackEvent details in SlackRequest
-type SlackEvent struct {
-	Type     string     `json:"type"`
-	User     string     `json:"user"`
-	Item     ItemStruct `json:"item"`
-	Reaction string     `json:"reaction"`
-	EventTs  string     `json:"event_ts"`
-}
-
 // SlackRequest is parsed from incoming requests
 type SlackRequest struct {
-	Token       string     `json:"token"`
-	Challenge   string     `json:"challenge"`
-	TeamID      string     `json:"team_id"`
-	APIAppID    string     `json:"api_app_id"`
-	Event       SlackEvent `json:"event"`
-	EventType   string     `json:"type"`
-	EventID     string     `json:"event_id"`
-	EventTime   int        `json:"event_time"`
-	AuthedUsers []string   `json:"authed_users"`
+	Token     string `json:"token"`
+	Challenge string `json:"challenge"`
+	TeamID    string `json:"team_id"`
+	APIAppID  string `json:"api_app_id"`
+	Event     struct {
+		Type string `json:"type"`
+		User string `json:"user"`
+		Item struct {
+			Type    string `json:"type"`
+			Channel string `json:"channel"`
+			Ts      string `json:"ts"`
+		} `json:"item"`
+		Reaction string `json:"reaction"`
+		EventTs  string `json:"event_ts"`
+	} `json:"event"`
+	EventType   string   `json:"type"`
+	EventID     string   `json:"event_id"`
+	EventTime   int      `json:"event_time"`
+	AuthedUsers []string `json:"authed_users"`
 }
 
 func handler(reaction SlackRequest) (events.APIGatewayProxyResponse, error) {
